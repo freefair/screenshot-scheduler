afce543a9fd8a28a632fe35253a1587a206ceee0
Dennis Fricke <d.fricke-extern@fielmann.com>
2019-05-24 11:49:44 +0200

    # fixes form login password

--- src/main/java/io/freefair/screenshot_scheduler/models/FormAuthenticationInformation.java
+++ src/main/java/io/freefair/screenshot_scheduler/models/FormAuthenticationInformation.java
@@ -1,22 +1,40 @@
 package io.freefair.screenshot_scheduler.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
-@Data
 public class FormAuthenticationInformation extends AuthenticationInformation implements UsernamePasswordAuthenticationInformation {
 
+	@Getter
+	@Setter
 	private String usernameSelector;
+	@Getter
+	@Setter
 	private String passwordSelector;
+	@Getter
+	@Setter
 	private String submitSelector;
-
+	@Getter
+	@Setter
 	private String username;
 
-	@JsonIgnore
 	private String password;
+
+	@JsonIgnore
+	public String getPassword() {
+		return this.password;
+	}
+
+	@JsonProperty("password")
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
